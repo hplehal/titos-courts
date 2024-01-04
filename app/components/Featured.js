1f4d6d34c57d26c1db9c496b2caa07ed64812f26
Tej Lehal <tejlehal@Tejs-MacBook-Pro.local>
2024-01-04 02:40:28 -0500

    Added the teams and carousel

--- app/components/Featured.js
+++ app/components/Featured.js
@@ -1,18 +1,52 @@
 import Link from "next/link";
 import Image from 'next/image';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import styles from './Featured.module.css';
+import '@splidejs/react-splide/css';
 
 export default function Featured() {
+  const teams = ['hamchoi', 'hisym', 'jujutsuspikesen', 'laangels', 'lefthand', 'nbn', 'onlyfrans', 'ouubae', 'samsclub', 'serveivor', 'shrek5', 'skg', 'spam', 'stepkuya', 'strawhats', 'tippics', 'wufang', 'wwfs'];
     return(
       <section className={styles.section}>
-        <h3>Season 1 Teams</h3>
-        <div>
-            <ul>
-                <li>
-                    <h1></h1>
-                </li>
-            </ul>
-        </div>
+        <h3><a><Image 
+            src="/tvl.svg"
+            alt="Titos Volleyball Logo"
+            width={100}
+            height={100}
+            priority
+        /></a>Winter Season Teams
+        <a><Image 
+            src="/tvl.svg"
+            alt="Titos Volleyball Logo"
+            width={100}
+            height={100}
+            priority
+        /></a></h3>
+        <Splide  options={ {
+            rewind : true,
+            perPage: 6,
+            height : '90px',
+            gap    : '.5rem',
+            pagination : false
+          } }>
+        {/* <div class={styles.teamSlider}>
+          <div class={styles.teamsItemList}> */}
+            {teams.map((item, index) => {
+              return (
+                <SplideSlide key={index}>
+                  <a><Image 
+                      src={`/season2Teams/${item}.png`}
+                      alt="Titos Volleyball Logo"
+                      width={150}
+                      height={90}
+                      priority
+                  /></a>
+                </SplideSlide>
+              )
+            })}
+          {/* </div>
+        </div> */}
+        </Splide>
       </section>
     );
 }
--- app/components/Hero.js
+++ app/components/Hero.js
@@ -5,10 +5,10 @@ import styles from './Hero.module.css';
 export default function Hero() {
     return(
       <section className={styles.section}>
-        <h1>Register Now</h1>
+        <h1>Winter Season</h1>
         <div>
-            <p>Our Winter Season Registration is now open!</p>
-            <p>The Best COED Volleyball League in Mississauga</p>
+            <p>Our Winter Season starts January 9th!</p>
+            <p>The Best Adult COED Volleyball League in Mississauga</p>
         </div>
         <Link href={'/register'} passHref><button component="a">Register</button></Link>
         

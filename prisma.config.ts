be3da8f212b0592bddac2fc0ba34f91bff7eaa0f
hplehal <tejlehal13@gmail.com>
2026-03-19 20:52:29 -0400

    set up db fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,9 +4,6 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
-  migrations: {
-    path: path.join("prisma", "migrations"),
-  },
   datasource: {
     url: env("DATABASE_URL"),
   },

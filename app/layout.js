bb78f3ab9c7e81944e8433f7d4d04d318d717427
hplehal <tejlehal13@gmail.com>
2026-04-11 00:53:51 -0400

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter, Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -44,6 +46,8 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
